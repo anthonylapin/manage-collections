bc4c455dab3e3fcd3b2e4aa38814adf5c9e8ef13
Anton Lapin <antonlapin@Antons-MacBook-Pro.local>
2020-08-20 09:47:31 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const app = express()
+const http = require('http')
+const server = http.createServer(app)
+const mongoose = require('mongoose')
+const config = require('config')
+
+const PORT = process.env.PORT || config.get('port') || 5000
+const MONGODB_URI = config.get('mongoUri')
+
+const connectToDatabase = async () => {
+    try {
+        await mongoose.connect(MONGODB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        })
+        console.log('Database connected successfully')
+    } catch (e) {
+        throw e
+    }
+}
+
+const listenServer = () => {
+    server.listen(PORT, () => {
+        console.log(`Server is on port ${PORT}`)
+    })
+}
+
+const start = () => {
+    try {
+        connectToDatabase()
+        listenServer()
+    } catch (e) {
+        console.log(`Server error: ${e.message}`)
+        process.exit(1)
+    }
+}
+
+start()
